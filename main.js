a728c3086e1b8940ffef7b04b0d98a0410d68e4a
Moe <momo_yuzu_77@yahoo.co.jp>
2021-04-28 12:57:16 -0700

    add new file

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ var day = new Date().getDay();
       	sayday = "Saturday";
       } 
       else {
-      	sayday = "Day not found";
+      	sayday = "not found";
       }
       
 
@@ -86,50 +86,3 @@ var day = new Date().getDay();
 }
 
 
-/*Variables*/
-
-let myvariable = "Moe"
-
-document.getElementById("test").innerHTML = myvariable;
-
-/*operator*/
-
-document.getElementById('test1').innerHTML = (5 + 6) *10;
-
-var x, y;
-x = 5;
-y = 6;
-
-document.getElementById("test2").innerHTML = x + y;
-
-/*function*/
-
-var z = myFunction (4, 3);
-document.getElementById("test3").innerHTML = z;
-
-function myFunction (p, q) {
-	return p * q;
-}
-
-/*Function Temperature*/
-
-function toCelsius (fahrenheit) {
-	return (5/9) * (farenheit-32);
-
-
-}
-document.getElementById("test4").innerHTML = toCelsius(77);
-
-/*condition*/
-
-function myFunction1() {
-      var hour = new Date().getHours();
-      var greeting;
-      if (hour < 18) {
-      	greeting = "Good Day";
-      } else {
-      	greeting = "Good evening";
-      }
-      document.getElementById("test5").innerHTML = greeting;
-
-}
